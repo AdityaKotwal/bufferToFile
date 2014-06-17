b2892d76dfc5422a7e270bde980c285bc7cad296
Aditya Kotwal <akotwal@pa-dbc1121.eng.vmware.com>
2014-06-17 12:54:35 -0700

    Replaces strcpy by Util_Memcpy32

--- bufferToFile_ESX.c
+++ bufferToFile_ESX.c
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "str.h"
+#include "util.h"
 #define DST "/testOutDir/dump2"
 #define MAX_BLKS 1024
 #define SIZE_BLK 4096 // Each block is 4K
@@ -49,7 +50,7 @@ unsigned long
    op = fopen(DST,"ab");
    buf_pos = 0;
    while(buf_pos < len){
-      Str_Strcpy(hash, buf + buf_pos, 32);
+      Util_Memcpy32(hash, buf + buf_pos, 32);
       hash[16 * 2] = '\0'; // Null character to end string
       fprintf(op, "%s,%s,%lu\n", UID, hash, offset);
       offset += SIZE_BLK;
@@ -72,8 +73,8 @@ unsigned long
 void testCase(){
    char buf[100];
    char uid[100];
-   Str_Strcpy(buf,"1234567891123456123456789012345621345678911234561234567890123456",64);
-   Str_Strcpy(uid,"abc",3);
+   Str_Strcpy(buf,"1234567891123456123456789012345621345678911234561234567890123456",100);
+   Str_Strcpy(uid,"abc",10);
    bufferToFile(buf,64,uid,1);
 }
 
